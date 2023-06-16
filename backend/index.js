12c5d177943408958a8f903b193bc77d0b8940ec
kiminjoong <ijboym216@gmail.com>
2023-06-16 10:31:31 +0900

    1. Node.js로 ChatGPT API 활용하기

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,7 +11,12 @@ const openai = new OpenAIApi(configuration);
 async function apiCall() {
   const chatCompletion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello world" }],
+    // max_tokens: 100,
+    // temperature: 0.5,
+    messages: [
+      { role: "system", content: "Hello world" },
+      { role: "user", content: "Hello world" },
+    ],
   });
 
   console.log(chatCompletion.data.choices[0].text);

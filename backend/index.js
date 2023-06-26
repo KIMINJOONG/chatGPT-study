3a1325982f883d4bf85cb54e0d05053c91e50d6a
kiminjoong <ijboym216@gmail.com>
2023-06-26 16:15:55 +0900

    13. 여러 건의 채팅 메세지 연결하기

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/fortuneTell", async function (req, res) {
+  let { userMessages, assistantMessages } = req.body;
+  let messages = [
+    { role: "system", content: "당신은 세계최고의 점성술사입니다." },
+    { role: "user", content: req.body.message },
+  ];
+
+  while (userMessages.length != 0 && assistantMessages.length != 0) {
+    if (userMessages.length != 0) {
+      messages.push(
+        JSON.parse(
+          `{"role": "user", "content": ${userMessages
+            .shift()
+            .replace(/\n/g, "")}},`
+        )
+      );
+    }
+
+    if (assistantMessages.length != 0) {
+      messages.push(
+        JSON.parse(
+          `{"role": "user", "content": ${assistantMessages
+            .shift()
+            .replace(/\n/g, "")}},`
+        )
+      );
+    }
+  }
   const chatCompletion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     // max_tokens: 100,
     // temperature: 0.5,
-    messages: [
-      { role: "system", content: "당신은 세계최고의 점성술사입니다." },
-      { role: "user", content: req.body.message },
-    ],
+    messages: messages,
   });
+
   let fortune = chatCompletion.data.choices[0].message["content"];
   console.log(fortune);
   res.json({ assistant: fortune });
